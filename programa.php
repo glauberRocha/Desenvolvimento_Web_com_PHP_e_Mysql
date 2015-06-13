4300f80f2b848d4da0f2bb617f8e49ac5e309041
Glauber Rocha <glauber.roch7@gmail.com>
2015-06-13 16:08:00 -0300

    Meu primeiro programa PHP

---
+++ programa.php
@@ -0,0 +1,10 @@
+﻿<?php
+echo "Hoje é dia " . date('d F Y');
+echo " agora são " . date('H:i:s a');
+
+if(6 - date('w') == 0){
+	echo ", hoje é sábado!";
+}
+else{
+	echo ", faltam " . (6 - date('w'));
+}
